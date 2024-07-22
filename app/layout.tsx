b053ebe78b512c9a433a7f256604e326959e26a8
ariqfrhan <muhammadariq3@gmail.com>
2024-07-22 14:52:14 +0700

    tes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,55 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ariq Farhan",
-  description: "Personal Portfolio for Muhammad Ariq Farhan",
+  title: "Ariq Farhan's Portfolio",
+  description:
+    "Welcome to the personal portfolio of Muhammad Ariq Farhan, a versatile software engineer, colorist, and RPA specialist with expertise in web and mobile app development, automation, and video editing.",
+  keywords: [
+    "Muhammad Ariq Farhan",
+    "software engineer",
+    "colorist",
+    "RPA specialist",
+    "web development",
+    "mobile app development",
+    "robotic process automation",
+    "video editing",
+    "portfolio",
+  ],
+  authors: [{ name: "Muhammad Ariq Farhan" }],
+  creator: "Muhammad Ariq Farhan",
+  publisher: "Muhammad Ariq Farhan",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: "Muhammad Ariq Farhan",
+    description:
+      "Personal portfolio showcasing expertise in software engineering, coloring, and RPA.",
+    url: "https://www.yourwebsite.com",
+    siteName: "Muhammad Ariq Farhan Portfolio",
+    images: [
+      {
+        url: "https://www.yourwebsite.com/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Muhammad Ariq Farhan Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Muhammad Ariq Farhan | Software Engineer & Colorist",
+    description:
+      "Personal portfolio showcasing expertise in software engineering, coloring, and RPA.",
+    creator: "@ariqfrhan",
+    images: ["https://www.yourwebsite.com/twitter-image.jpg"],
+  },
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
 };
 
 export default function RootLayout({
